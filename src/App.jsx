1ecc0e476b9ec74692e9228bf5f473ffde342c8a
tictools <jaumevillarreal@gmail.com>
2024-02-06 22:18:02 +0100

    feat: add state to App and define Counter + add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from "react";
-
+import "./App.css";
+import { Counter } from "./Counter";
 const App = () => {
-  // save each button click in its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [score, setScore] = useState(0);
 
-  return <div>code here</div>;
+  const handleIncrease = () => {
+    setScore((prevScore) => {
+      return prevScore + 1;
+    });
+  };
+
+  const handleDecrease = () => {
+    setScore((prevScore) => {
+      return prevScore - 1;
+    });
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>State management</h1>
+      <div className="counter__card">
+        <button
+          className="counter__button counter__button--primary"
+          onClick={handleIncrease}
+        >
+          Increase
+        </button>
+        <Counter count={score} />
+        <button
+          onClick={handleDecrease}
+          className="counter__button counter__button--secondary"
+        >
+          Decrease
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
